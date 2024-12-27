afe0e980f2e81ce890035525e7e080c9ed5f480c
nahead <gplaying780@gmail.com>
2024-12-27 08:36:26 -0800

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div id='about' className='container pt-36'>
       <h2 className='text-4xl md:text-5xl'data-aos="zoom-in-up">About Me</h2>
-      <p className='text-white pt-4'data-aos="zoom-in-up">
+      <p className=' pt-4'data-aos="zoom-in-up">
         I am a student at GIAIC, pursuing a course in Artificial Intelligence, Web 3.0, & Metaverse.
         Passionate about tectnology and constantly learning new skills to stay up-to-date with the latest innvovations.
       </p>
