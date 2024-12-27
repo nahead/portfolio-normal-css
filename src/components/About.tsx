2b40b2dc4186e562c5f0f2809d7a951de66300d2
nahead <gplaying780@gmail.com>
2024-12-27 08:33:13 -0800

    first commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id='about' className='container pt-32'>
+    <div id='about' className='container pt-36'>
       <h2 className='text-4xl md:text-5xl'data-aos="zoom-in-up">About Me</h2>
       <p className='text-white pt-4'data-aos="zoom-in-up">
         I am a student at GIAIC, pursuing a course in Artificial Intelligence, Web 3.0, & Metaverse.
