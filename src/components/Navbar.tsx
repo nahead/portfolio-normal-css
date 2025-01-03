0229cc85df3712f108c9756bd9c21b1a23f6b5c3
nahead <gplaying780@gmail.com>
2025-01-03 00:45:39 -0800

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ const Navbar = () => {
   }
 
   return (
-<<<<<<< HEAD
     <div className='container'>
       <div className='navbar'>
         <div className='navbar-title'>Nahead</div>
@@ -22,17 +21,6 @@ const Navbar = () => {
           <li className='menuLink'><a href="#projects">Projects</a></li>
           <li className='menuLink'><a href="#skills">Skills</a></li>
           <li className='menuLink'><a href="#contact">Contact</a></li>
-=======
-    <div className='container pt-8'>
-       <div className=' flex justify-between items-center'>
-        <div className='text-xl font-medium'> Nahead</div>
-        <ul className='gap-10 lg:gap-16 hidden md:flex'>
-            <li className='menuLink'><a href="#hero">Home</a></li>
-            <li className='menuLink'><a href="#about">About</a></li>
-            <li className='menuLink'><a href="#projects">Projects</a></li>
-            <li className='menuLink'><a href="#skills">Skills</a></li>
-            <li className='menuLink'><a href="#contact">Contact</a></li>
->>>>>>> 74596c0f6652a18aa842e163fadfa8b652d03bca
         </ul>
         <div className='menu-icon' onClick={togglemenu}>
           {isMenuOpen ? <AiOutlineClose size={29} /> : <MdOutlineMenu size={29} />}
